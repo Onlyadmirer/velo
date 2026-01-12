e019ea442c4967325a3f608cf3316cb602748243
Onlyadmirer <akmalrbc6@gmail.com>
2026-01-12 20:29:04 +0800

    Fetches and displays user profile data
    Implements user data fetching on application load.
    
    The application now retrieves user data upon initialization and displays the user's name in the profile dropdown. This enhances user experience by displaying personalized information.

--- components/fragments/DropdownProfile.tsx
+++ components/fragments/DropdownProfile.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useAuth } from "@/app/auth/useAuth";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
@@ -17,10 +18,11 @@ interface DropdownProfileProps {
 }
 
 function DropdownProfile({ className, align }: DropdownProfileProps) {
+  const { user } = useAuth();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className='flex flex-row items-center gap-1'>
-        <h1>Akmal</h1>
+        <h1>{user ? user.name : ""}</h1>
         <IoIosArrowDown />
       </DropdownMenuTrigger>
       <DropdownMenuContent className={`${className}`} align={align}>
@@ -30,7 +32,7 @@ function DropdownProfile({ className, align }: DropdownProfileProps) {
               <AvatarImage src='https://github.com/shadcn.png' />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <h1>Akmal</h1>
+            <h1>{user ? user.name : ""}</h1>
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
